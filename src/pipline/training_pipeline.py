c99fa37f70b625162ed754aeacc07ff62519d066
Shalha-Mucha18 <ms.mucha18@gmail.com>
2026-02-27 23:21:58 +0600

    added traning pipeline

--- src/pipline/training_pipeline.py
+++ src/pipline/training_pipeline.py
@@ -1,6 +1,7 @@
 import sys
 from src.exception import MyException
 from src.logger import logging
+from pandas import DataFrame
 
 from src.entity.config_entity import DataIngestionConfig
 from src.entity.artifact_entity import DataIngestionArtifact
@@ -14,6 +15,13 @@ from src.components.data_transformation import DataTransformation
 from src.entity.config_entity import ModelTrainerConfig
 from src.entity.artifact_entity import ModelTrainerArtifact
 from src.components.model_trainer import ModelTrainer
+from src.entity.config_entity import ModelEvaluationConfig
+from src.entity.artifact_entity import ModelEvaluationArtifact
+
+from src.entity.config_entity import ModelPusherConfig
+from src.entity.artifact_entity import ModelPusherArtifact
+from src.components.model_pusher import ModelPusher
+from src.components.model_evaluation import ModelEvaluation
 
 
 
@@ -24,6 +32,9 @@ class TrainPipeline:
         self.data_validation_config = DataValidationConfig()
         self.data_transformation_config = DataTransformationConfig()
         self.model_trainer_config = ModelTrainerConfig()
+        self.model_evaluation_config = ModelEvaluationConfig()
+        self.model_pusher_config = ModelPusherConfig()
+
 
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
@@ -96,11 +107,30 @@ class TrainPipeline:
             return model_trainer_artifact
 
         except Exception as e:
+            raise MyException(e, sys)   
+
+    def start_model_evaluation(self, model_trainer_artifact: ModelTrainerArtifact, data_transformation_artifact: DataTransformationArtifact,data_ingestion_artifact: DataIngestionArtifact) -> ModelEvaluationArtifact:             
+        try:
+            model_evaluation = ModelEvaluation(model_eval_config=self.model_evaluation_config,
+                                               data_ingestion_artifact=data_ingestion_artifact,
+                                               model_trainer_artifact=model_trainer_artifact)
+            model_evaluation_artifact = model_evaluation.initiate_model_evaluation()
+            return model_evaluation_artifact
+        except Exception as e:
+            raise MyException(e, sys)
+    
+    def start_model_pusher(self, model_evaluation_artifact: ModelEvaluationArtifact) -> ModelPusherArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting model pushing
+        """
+        try:
+            model_pusher = ModelPusher(model_evaluation_artifact=model_evaluation_artifact,
+                                       model_pusher_config=self.model_pusher_config
+                                       )
+            model_pusher_artifact = model_pusher.initiate_model_pusher()
+            return model_pusher_artifact
+        except Exception as e:
             raise MyException(e, sys)        
-            
-
-
-
     def run_pipeline(self):
         try:
             logging.info("Entered the run_pipeline method of TrainPipeline class")
@@ -119,10 +149,36 @@ class TrainPipeline:
             model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
             logging.info("Model trainer component completed")
             logging.info("Exited the run_pipeline method of TrainPipeline class")
-        except Exception as e:
-            raise MyException(e, sys) from e        
-            
-            
+            logging.info("Starting the model evaluation component")
+            model_evaluation_artifact = self.start_model_evaluation(model_trainer_artifact=model_trainer_artifact,
+                                                                   data_transformation_artifact=data_transformation_artifact,
+                                                                   data_ingestion_artifact=data_ingestion_artifact)
+            logging.info("Model evaluation component completed")
+            logging.info("Exited the run_pipeline method of TrainPipeline class")
 
+            if not model_evaluation_artifact.is_model_accepted:
+                logging.info(f"Model not accepted.")
+                return None
+            model_pusher_artifact = self.start_model_pusher(model_evaluation_artifact=model_evaluation_artifact)
+
+        except Exception as e:
+            raise MyException(e, sys) from e   
+    def predict(self,data:DataFrame)->DataFrame:
+        """
+        This method of TrainPipeline class is responsible for prediction
+        Returns: Predictions in string format
+        """  
+        try:
+            logging.info("Entered the predict method of TrainPipeline class")
+            model = Proj1Estimator(
+                  bucket_name=self.prediction_pipeline_config.bucket_name,
+                  model_path=self.prediction_pipeline_config.model_path
+
+            )
+            result = model.predict(data)
+            logging.info("Exited the predict method of TrainPipeline class")
+            return result
             
+        except Exception as e:
+            raise MyException(e, sys) from e
 
