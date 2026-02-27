b89f12782fde886c930afd446cc5c4fbe7dfdc8f
Shalha-Mucha18 <ms.mucha18@gmail.com>
2026-02-27 23:21:43 +0600

    added prediction pipeline

---
+++ src/pipline/prediction_pipeline.py
@@ -0,0 +1,106 @@
+import sys
+from src.entity.config_entity import VehiclePredictorConfig
+from src.entity.s3_estimator import Proj1Estimator
+from src.logger import logging
+from src.exception import MyException
+from pandas import DataFrame
+
+
+class VehicleData:
+    def __init__(self,
+                Gender,
+                Age,
+                Driving_License,
+                Region_Code,
+                Previously_Insured,
+                Annual_Premium,
+                Policy_Sales_Channel,
+                Vintage,
+                Vehicle_Age_lt_1_Year,
+                Vehicle_Age_gt_2_Years,
+                Vehicle_Damage_Yes
+                ):
+
+        """
+        Vehicle Data constructor
+        Input: all features of the trained model for prediction
+        """ 
+
+        try:
+            self.Gender = Gender
+            self.Age = Age
+            self.Driving_License = Driving_License
+            self.Region_Code = Region_Code
+            self.Previously_Insured = Previously_Insured
+            self.Annual_Premium = Annual_Premium
+            self.Policy_Sales_Channel = Policy_Sales_Channel
+            self.Vintage = Vintage
+            self.Vehicle_Age_lt_1_Year = Vehicle_Age_lt_1_Year
+            self.Vehicle_Age_gt_2_Years = Vehicle_Age_gt_2_Years
+            self.Vehicle_Damage_Yes = Vehicle_Damage_Yes
+
+        except Exception as e:
+            raise MyException(e, sys) from e   
+
+    def get_vehicle_input_data_frame(self)-> DataFrame:  
+        """
+        This function returns a DataFrame from VehicleData class input
+        """  
+        try:
+            vehicle_input_dict = self.get_vehicle_data_as_dict()
+            return DataFrame(vehicle_input_dict)
+        except Exception as e:
+            raise MyException(e, sys) from e
+    def get_vehicle_data_as_dict(self)-> dict:  
+        """
+        This function returns a dictionary from VehicleData class input
+        """  
+        try:
+            vehicle_input_dict = {
+                "id": [0],
+                "Gender":[self.Gender],
+                "Age":[self.Age],
+                "Driving_License":[self.Driving_License],
+                "Region_Code":[self.Region_Code],
+                "Previously_Insured":[self.Previously_Insured],
+                "Annual_Premium":[self.Annual_Premium],
+                "Policy_Sales_Channel":[self.Policy_Sales_Channel],
+                "Vintage":[self.Vintage],
+                "Vehicle_Age_lt_1_Year":[self.Vehicle_Age_lt_1_Year],
+                "Vehicle_Age_gt_2_Years":[self.Vehicle_Age_gt_2_Years],
+                "Vehicle_Damage_Yes":[self.Vehicle_Damage_Yes]
+            }
+            return vehicle_input_dict
+        except Exception as e:
+            raise MyException(e, sys) from e
+
+class VehicleDataClassifier:
+    def __init__(self, prediction_pipeline_config: VehiclePredictorConfig = VehiclePredictorConfig()) -> None:
+        """
+        :param prediction_pipeline_config: Configuration for prediction
+        """
+        try:
+            self.prediction_pipeline_config = prediction_pipeline_config
+        except Exception as e:
+            raise MyException(e, sys) from e
+
+    def predict(self, dataframe: DataFrame) -> str:
+        """
+        This is the method of VehicleDataClassifier
+        Returns: Prediction in string format
+        """
+        try:
+            logging.info("Entered predict method of VehicleDataClassifier class")
+            model = Proj1Estimator(
+                bucket_name=self.prediction_pipeline_config.model_bucket_name,
+                model_path=self.prediction_pipeline_config.model_file_path,
+            )
+            result = model.predict(dataframe)
+            logging.info("Exited predict method of VehicleDataClassifier class")
+            return result
+        except Exception as e:
+            raise MyException(e, sys) from e
+
+    
+
+
